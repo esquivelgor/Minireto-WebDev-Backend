52004fa745432bf11b1f798d33c3e2454de38ad7
esquigor <esquivelguillerm@gmail.com>
2023-02-28 19:57:29 -0600

    V1.0.0

--- Act3/BackEnd/Server/index.js
+++ Act3/BackEnd/Server/index.js
@@ -1,9 +1,14 @@
-// server/index.js
 
 const express = require("express"); // We add the "library" express
+
 const PORT = process.env.PORT || 3001; // We define the port number
 const app = express(); // Create the layout
 
+app.get("/api", (req, res) => {
+    res.json("This is gonna be just a message :)")
+});
+
+// Callback to open the port
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
