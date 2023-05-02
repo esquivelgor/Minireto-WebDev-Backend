463be21fe7206add3e18aa241309facacbdb6641
esquivelgor <esquivelguillerm@gmail.com>
2023-05-02 12:34:14 -0600

    add cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import employeesRoutes from './routes/employees.routes.js'
 import commentsRoutes from './routes/comments.routes.js' 
 import indexRoutes from './routes/index.routes.js'
@@ -6,6 +7,7 @@ import { PORT } from './config.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use(indexRoutes)
